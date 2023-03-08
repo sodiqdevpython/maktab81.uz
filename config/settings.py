9d8892f274590b7358669b23fa752c319fbbd7b6
Sodiq <sodiq7898@gmail.com>
2023-03-09 00:32:56 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -129,9 +129,14 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
