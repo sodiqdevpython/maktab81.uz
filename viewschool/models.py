a2ffcada149c8076ff2b24378179a5fef98d1751
Sodiq <sodiq7898@gmail.com>
2023-04-04 20:39:28 +0500

    er

--- viewschool/models.py
+++ viewschool/models.py
@@ -17,7 +17,7 @@ class SchoolMap(models.Model):
 
 class SchoolStaffs(models.Model):
     fio = models.CharField(max_length=50, verbose_name="To'liq ismi: ")
-    person = models.CharField(max_length=100, verbose_name="Ko'proq ma'lumot: ", help_text="Masalan: 81-maktab direktori Oloviddin Ochilov")
+    person = models.TextField(verbose_name="Ko'proq ma'lumot: ", help_text="Masalan: 81-maktab direktori Oloviddin Ochilov")
     img_person = models.ImageField(null=False, blank=False, upload_to='images/', verbose_name='Rasm: ')
     web_url = models.URLField(verbose_name="Bog'lanish uchun silkani kiriting", help_text="Agar ushbu odamning veb sahifasi yo'q bo'lsa buni kiriting: https://samshmaktab81.uz")
 
