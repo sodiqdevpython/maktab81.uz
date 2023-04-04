5f37267c3e14f42b76c785ed200833b9f6630379
Sodiq <sodiq7898@gmail.com>
2023-04-04 19:49:25 +0500

    dvnie

--- config/settings.py
+++ config/settings.py
@@ -150,6 +150,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
