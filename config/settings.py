4298b7b425b3e3c0892d8b0834ddb4952683570b
Sodiq <sodiq7898@gmail.com>
2023-04-04 22:50:58 +0500

    ewrvfgergw

--- config/settings.py
+++ config/settings.py
@@ -175,7 +175,8 @@ if DEBUG:
     ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
