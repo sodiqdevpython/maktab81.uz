fa47e5ea6b980183e18898e872a21e6dceae9383
Sodiq <sodiq7898@gmail.com>
2023-04-06 22:32:14 +0500

    vuhfduvber

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nh=!u8_7h)i^k)f_iomz!guxy(59gr8+*qz6ce#d^+f(=&1!dc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
-ALLOWED_HOSTS = ['164.90.232.10','samshmaktab81.uz','www.samshmaktab81.uz']
+ALLOWED_HOSTS = ['164.90.232.10','www.samshmaktab81.uz','samshmaktab81.uz']
 
 # Application definition
 
