828c27a7ae4a850d6499be5bfe68401b52f2bba8
Sodiq <sodiq7898@gmail.com>
2023-04-07 15:57:30 +0500

    cdjrbvuejr

--- config/settings.py
+++ config/settings.py
@@ -183,7 +183,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static/"    
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
