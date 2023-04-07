f7e7d74b1f2383a666274363f115b228ae2a377b
Sodiq <sodiq7898@gmail.com>
2023-04-07 17:05:11 +0500

    vtrgbrt

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nh=!u8_7h)i^k)f_iomz!guxy(59gr8+*qz6ce#d^+f(=&1!dc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ['127.0.0.1','164.90.232.10','www.samshmaktab81.uz','samshmaktab81.uz']
+ALLOWED_HOSTS = ['164.90.232.10','www.samshmaktab81.uz','samshmaktab81.uz']
 # Application definition
 
 INSTALLED_APPS = [
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'namuna',
-#         'USER': 'sodiq',
-#         'PASSWORD': 'sodiq',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
-#                          For ofline
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'namuna',
+        'USER': 'sodiq',
+        'PASSWORD': 'sodiq',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+#                          For ofline
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
                         #   For ofline
 
 # Password validation
