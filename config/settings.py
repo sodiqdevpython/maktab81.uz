a6fc1be78de04295e3dd7dd4a9b417f2f7e69980
sodiqdevpython <sodiqdevpython@gmail.com>
2024-09-23 14:30:28 +0500

    Fixed bugs with frontend2

--- config/settings.py
+++ config/settings.py
@@ -84,12 +84,12 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 52428800  # 50MB
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 DATABASES = {
         'default': {
